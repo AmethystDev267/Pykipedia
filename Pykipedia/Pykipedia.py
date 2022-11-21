33957d97123194f830fe4e59fda2774f363adef7
AmethystDev267 <118764555+AmethystDev267@users.noreply.github.com>
2022-11-21 20:20:00 +0000

    Add files via upload

--- Pykipedia/Pykipedia.py
+++ Pykipedia/Pykipedia.py
@@ -5,4 +5,5 @@ print("Welcome to Pykipedia, this is a project made by Amethyst and open sourced
 textinput = input("")
 if (textinput) == "Python":print("Python is a coding language made for the purposes of a human readable coding language while also being a powerful coding language, on its own without any modules it only has a couple of coding expressions but with some modules imported it quickly becomes very powerful.")
 if (textinput) == "Secret":print("YAHAHA, You found me, im a secret left by amethyst in the code if you want to add to this for people in the future just continue this sentence.")
+if (textinput) == "Flute":print("funny coding thing")
 time.sleep(999999999999)
