02d71af45e2f1f1613f22ee9a2194477e9577fb3
AmethystDev267 <118764555+AmethystDev267@users.noreply.github.com>
2022-11-21 18:19:04 +0000

    Current Pykipedia
    NOTES
    It needs a way to fetch search requests instead of them being stored inside of the py file.

---
+++ Pykipedia/Pykipedia.py
@@ -0,0 +1,8 @@
+import time
+
+print("Welcome to Pykipedia, this is a project made by Amethyst and open sourced on github, hope you enjoy this little project")
+
+textinput = input("")
+if (textinput) == "Python":print("Python is a coding language made for the purposes of a human readable coding language while also being a powerful coding language, on its own without any modules it only has a couple of coding expressions but with some modules imported it quickly becomes very powerful.")
+if (textinput) == "Secret":print("YAHAHA, You found me, im a secret left by amethyst in the code if you want to add to this for people in the future just continue this sentence.")
+time.sleep(999999999999)
